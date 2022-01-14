0e199e1632a63355944460135f080e9addb08a17
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2022-01-15 00:14:57 +0300

    added css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-
-import './App.css';
+import "./App.css";
+import { Routes, Route, NavLink } from "react-router-dom";
+import { PokemonList } from "./components/PokemonList";
+import { Pokemon } from "./components/Pokemon";
 
 function App() {
   return (
     <div className="App">
-     
+      <nav>
+        <NavLink to={"/"}>Search</NavLink>
+      </nav>
+      <Routes>
+        <Route path="/" element={<PokemonList />} />
+        <Route path="/pokemon/:pokemon" element={<Pokemon />} />
+      </Routes>
     </div>
   );
 }
