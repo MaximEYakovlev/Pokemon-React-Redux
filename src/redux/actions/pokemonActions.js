aea3cf7968df2c31a3557d286c50297edeec123a
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2022-01-15 22:19:49 +0300

    added redux action getPokemon

--- src/redux/actions/pokemonActions.js
+++ src/redux/actions/pokemonActions.js
@@ -3,6 +3,9 @@ import {
   POKEMON_LIST_LOADING,
   POKEMON_LIST_SUCCESS,
   POKEMON_LIST_FAIL,
+  POKEMON_MULTIPLE_LOADING,
+  POKEMON_MULTIPLE_SUCCESS,
+  POKEMON_MULTIPLE_FAIL,
 } from "../types";
 
 export const GetPokemonList = (page) => async (dispatch) => {
@@ -22,6 +25,26 @@ export const GetPokemonList = (page) => async (dispatch) => {
   } catch (e) {
     dispatch({
       type: POKEMON_LIST_FAIL,
+    });
+  }
+};
+
+export const getPokemon = (pokemon) => async (dispatch) => {
+  try {
+    dispatch({
+      type: POKEMON_MULTIPLE_LOADING,
+    });
+
+    const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+
+    dispatch({
+      type: POKEMON_MULTIPLE_SUCCESS,
+      payload: res.data,
+      pokemonName: pokemon,
+    });
+  } catch (e) {
+    dispatch({
+      type: POKEMON_MULTIPLE_FAIL,
     });
   }
 };
